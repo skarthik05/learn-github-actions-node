44edd7cda3b6893b6f2f7b56db0d0477ba1b7e68
skarthik05 <karthiks0598@gmail.com>
2025-04-06 17:20:04 +0530

    test case should fail and merge button should be "disabled"

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,7 @@ const sum = require("./sum");
 test("adds 5 + 7 to equal 12", () => {
   expect(sum(5, 7)).toBe(12);
 });
+
+test("adds 5 + 7 to equal 11 and should fail", () => {
+  expect(sum(5, 7)).toBe(11);
+});
